0ca824bc23abb49fdeedd4b7ef2325288ca1d444
caohongming001 <caohongming001@gmail.com>
2016-08-25 01:04:48 +0800

    springthread

--- date/src/main/java/org/chmcc/util/DateUtil.java
+++ date/src/main/java/org/chmcc/util/DateUtil.java
@@ -1,17 +1,26 @@
 package org.chmcc.util;
 
 import java.util.Calendar;
-import java.util.Date;
 
 /**
  * Created by caohm on 2016/8/24.
  */
 public class DateUtil {
-    public static boolean isDawn() {
-        boolean isDawn = false;
+    /**
+     * 判断是否是第二天凌晨
+     *
+     * @param offset 时间偏移量 Millis
+     * @return boolean
+     */
+    public static boolean isDawn(int offset) {
         Calendar cal = Calendar.getInstance();
-        Date time = cal.getTime();
-        System.out.println(time.getTime());
-        return isDawn;
+        long current = cal.getTimeInMillis();
+        cal.add(Calendar.DAY_OF_MONTH, 1);
+        cal.set(Calendar.HOUR_OF_DAY, 1);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        long min = cal.getTimeInMillis() - offset;
+        long max = cal.getTimeInMillis() + offset;
+        return min <= current && max >= current;
     }
 }
--- date/src/main/java/org/chmcc/util/run.java
+++ date/src/main/java/org/chmcc/util/run.java
@@ -4,4 +4,9 @@ package org.chmcc.util;
  * Created by caohm on 2016/8/24.
  */
 public class run {
+
+    public static void main(String[] s) {
+        DateUtil.isDawn(5);
+    }
+
 }
