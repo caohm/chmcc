da189e117f3ddf52fac6dea61bbf23abf85e8891
caohongming <caohongming@jd.com>
2016-08-30 10:40:02 +0800

    提交

--- date/src/main/java/org/chmcc/util/DateUtil.java
+++ date/src/main/java/org/chmcc/util/DateUtil.java
@@ -1,6 +1,8 @@
 package org.chmcc.util;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by caohm on 2016/8/24.
@@ -9,18 +11,24 @@ public class DateUtil {
     /**
      * 判断是否是第二天凌晨
      *
-     * @param offset 时间偏移量 Millis
+     * @param offset 时间偏移量 MILLISECOND
      * @return boolean
      */
     public static boolean isDawn(int offset) {
+        long current = System.currentTimeMillis();
         Calendar cal = Calendar.getInstance();
-        long current = cal.getTimeInMillis();
         cal.add(Calendar.DAY_OF_MONTH, 1);
-        cal.set(Calendar.HOUR_OF_DAY, 1);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
         cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
         cal.set(Calendar.MILLISECOND, 0);
-        long min = cal.getTimeInMillis() - offset;
-        long max = cal.getTimeInMillis() + offset;
+        Date dawn = cal.getTime();
+        long min = dawn.getTime() - offset;
+        long max = dawn.getTime() + offset;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
+        System.out.println(sdf.format(new Date(min)));
+        System.out.println(sdf.format(new Date(current)));
+        System.out.println(sdf.format(new Date(max)));
         return min <= current && max >= current;
     }
 }
