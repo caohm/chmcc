4ab63cd82fbc5ce37db3cb7f6d94d2e7a91a0261
caohongming <caohongming@jd.com>
2016-09-01 13:48:09 +0800

    w

--- redis-client/src/main/java/org/chmcc/redis/RedisSetJava.java
+++ redis-client/src/main/java/org/chmcc/redis/RedisSetJava.java
@@ -2,21 +2,42 @@ package org.chmcc.redis;
 
 import redis.clients.jedis.Jedis;
 
-import java.util.List;
+import java.util.Set;
 
-public class RedisListJava {
-   public static void main(String[] args) {
-      //Connecting to Redis server on localhost
-       Jedis jedis = new Jedis("192.168.99.100", 32783);
-      System.out.println("Connection to server sucessfully");
-      //store data in redis list
-      jedis.lpush("tutorial-list", "Redis");
-      jedis.lpush("tutorial-list", "Mongodb");
-      jedis.lpush("tutorial-list", "Mysql");
-     // Get the stored data and print it
-     List<String> list = jedis.lrange("tutorial-list", 0 ,5);
-     for(int i=0; i<list.size(); i++) {
-       System.out.println("Stored string in redis:: "+list.get(i));
-     }
- }
+public class RedisSetJava {
+    public static void main(String[] args) {
+        //Connecting to Redis server on localhost
+        Jedis jedis = new Jedis("192.168.99.100", 32839);
+        System.out.println("Connection to server sucessfully");
+        //store data in redis set
+        String key = "pc_getCurrentOrder";
+        double score = 1;
+        String member = "千水金年";
+        jedis.zincrby(key, score, member);
+        jedis.zincrby(key, score, member);
+        jedis.zincrby(key, score, member);
+        jedis.zincrby(key, score, member);
+        member = "zhangsan";
+        jedis.zincrby(key, score, member);
+        jedis.zincrby(key, score, member);
+        jedis.zincrby(key, score, member);
+        member = "lishi";
+        jedis.zincrby(key, score, member);
+        member = "wangwu";
+        jedis.zincrby(key, score, member);
+        jedis.zincrby(key, score, member);
+        // Get the member's count and score in [0 ,$]
+        Long zcount = jedis.zcount(key, 1, 10000);
+        Long zcard = jedis.zcard(key);
+        System.out.println(key + "'s size : " + zcount + " | " + zcard);
+        // Get the stored data and print it
+        Set<String> set = jedis.zrange(key, 0, -1);
+        System.out.println(key + "'s child : ");
+        for (String str : set) {
+            System.out.print("        " + str);
+            Double rscore = jedis.zscore(key, str);
+            System.out.println("      score : " + rscore);
+        }
+
+    }
 }
